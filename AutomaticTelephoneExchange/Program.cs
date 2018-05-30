d493bb8695d7d6b4197676a29c4de3e97e28a92e
zirosun1 <haneange@gmail.com>
2018-05-30 13:13:17 +0300

    Add project files.

---
+++ AutomaticTelephoneExchange/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticTelephoneExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
