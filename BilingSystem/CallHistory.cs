a241663b6086cf8bd6c7b5f855cf4fe7bc4c5940
zirosun1 <haneange@gmail.com>
2018-06-12 09:27:44 +0300

    billing

---
+++ BilingSystem/CallHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilingSystem
+{
+    class CallHistory
+    {
+        public IList<CallRecord> CallRecords { get; private set; }
+        public CallHistory()
+        {
+            CallRecords = new List<CallRecord>();
+        }
+        public void AddRecordOfReport(CallRecord record)
+        {
+            CallRecords.Add(record);
+        }
+        public IList<CallRecord> GetRecords()
+        {
+            return CallRecords;
+        }
+    }
+}
---
+++ BilingSystem/CallRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BilingSystem.Enums;
+
+namespace BilingSystem
+{
+    class CallRecord
+    {
+        public TypeOfCall TypeOfCall { get; private set; }
+        public int Number { get; private set; }
+        public DateTime Date { get; private set; }
+        public DateTime Time { get; private set; }
+        public int Amount { get; private set; }
+        public CallRecord(TypeOfCall typeOfCall, int number, DateTime date, DateTime time, int amount)
+        {
+            TypeOfCall = typeOfCall;
+            Number = number;
+            Date = date;
+            Time = time;
+            Amount = amount;
+        }
+    }
+}
